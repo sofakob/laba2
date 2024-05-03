d08a3cf8501de2e96d27819c89c8b15aca834364
sofakob <hhitech628@gmail.com>
2024-05-04 02:27:01 +0300

    laba2

--- test_laba2.py
+++ test_laba2.py
@@ -5,7 +5,7 @@ import sys
 from laba2 import laba2val
 
 class TestPasswordValidator(unittest.TestCase):
-    def test_valid_password(self):
+    def test_invalid (self):
         test_input = "Pakmmklmmovgo542"
         expected_output = "You are smart person\n"
         with patch('builtins.input', return_value=test_input), \
@@ -13,7 +13,7 @@ class TestPasswordValidator(unittest.TestCase):
             laba2val(test_input)
             self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-    def test_short_password(self):
+    def test_corotci (self):
         test_input = "Jle4"
         expected_output = "Password must have more than 10 symbols\n"
         with patch('builtins.input', return_value=test_input), \
@@ -21,7 +21,7 @@ class TestPasswordValidator(unittest.TestCase):
             laba2val(test_input)
             self.assertEqual(mock_stderr.getvalue(), expected_output)
 
-    def test_no_digit_password(self):
+    def test_chistlnet (self):
         test_input = "Hkjrnfjnkjn"
         expected_output = "Password must have at least one digit\n"
         with patch('builtins.input', return_value=test_input), \
@@ -29,7 +29,7 @@ class TestPasswordValidator(unittest.TestCase):
             laba2val(test_input)
             self.assertEqual(mock_stderr.getvalue(), expected_output)
 
-    def test_no_uppercase_password(self):
+    def test_bucvinete (self):
         test_input = "njend62jkbkjle"
         expected_output = "Password must have at least one uppercase or lowercase letter\n"
         with patch('builtins.input', return_value=test_input), \
@@ -37,7 +37,7 @@ class TestPasswordValidator(unittest.TestCase):
             laba2val(test_input)
             self.assertEqual(mock_stderr.getvalue(), expected_output)
 
-    def test_no_lowercase_password(self):
+    def test_bucvimalenci (self):
         test_input = "JNKNKJNL266"
         expected_output = "Password must have at least one uppercase or lowercase letter\n"
         with patch('builtins.input', return_value=test_input), \
@@ -45,31 +45,31 @@ class TestPasswordValidator(unittest.TestCase):
             laba2val(test_input)
             self.assertEqual(mock_stderr.getvalue(), expected_output)
     
-    def test_exit_code_valid_password(self):
+    def test_pravilno (self):
         test_input = "KJNKLN45ewkd"
         with patch('builtins.input', return_value=test_input):
             exit_code = laba2val(test_input)
             self.assertEqual(exit_code, 0)
 
-    def test_exit_code_short_password(self):
+    def test_corotciinshiy (self):
         test_input = "Short1"
         with patch('builtins.input', return_value=test_input):
             exit_code = laba2val(test_input)
             self.assertEqual(exit_code, 1)
 
-    def test_exit_code_no_digit_password(self):
+    def test_pravcifr (self):
         test_input = "NoDigitPassword"
         with patch('builtins.input', return_value=test_input):
             exit_code = laba2val(test_input)
             self.assertEqual(exit_code, 1)
 
-    def test_exit_code_no_uppercase_password(self):
+    def test_mnogobol (self):
         test_input = "nopassword123"
         with patch('builtins.input', return_value=test_input):
             exit_code = laba2val(test_input)
             self.assertEqual(exit_code, 1)
 
-    def test_exit_code_no_lowercase_password(self):
+    def test_manuni(self):
         test_input = "NOPASSWORD123"
         with patch('builtins.input', return_value=test_input):
             exit_code = laba2val(test_input)
